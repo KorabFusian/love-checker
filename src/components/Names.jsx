84d8423166bbf42974d2084eb3aa67fb3dd461fb
Jeremi <jeremifk@gmail.com>
2021-10-01 18:53:39 +0200

    setup tailwind (+ rip off the playground)

--- src/components/Names.jsx
+++ src/components/Names.jsx
@@ -1,7 +1,14 @@
 const Names = () => {
   return (
-    <div>
-      <form></form>
+    <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+      <div className="absolute inset-3 bg-gradient-to-r from-pink-400 to-red-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl" />
+      <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <form>
+          <input type="text" />
+          <input type="text" />
+          <button type="submit" />
+        </form>
+      </div>
     </div>
   )
 }
